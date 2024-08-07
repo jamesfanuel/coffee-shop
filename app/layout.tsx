2ab4f0d46ee3ea0ae03ccc253135aca65f8598c0
jamesfanuelcinema21 <jamesfanuel@cinema21.co.id>
2024-08-07 19:33:12 +0700

    first init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,20 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Cormorant_Upright, Open_Sans } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
 
-const inter = Inter({ subsets: ["latin"] });
+const cormorantUpright = Cormorant_Upright({ 
+  subsets: ["latin"]
+  ,weight: ["300", "400", "500", "600", "700"] 
+  ,variable: "--font-cormorant_upright"
+});
+
+const openSans = Open_Sans({ 
+  subsets: ["latin"]
+  ,weight: ["300", "400", "500", "600", "700", "800"] 
+  ,variable: "--font-open_sans"
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +28,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${cormorantUpright.variable} ${openSans.variable}`}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
